145e72ee7dce8bc3f2f698393ea2e89955dbaf8f
MarriccZwei <marricc7@gmail.com>
2026-05-06 14:17:09 +0200

    more templates

---
+++ src/Drag/Fuselage.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+# Add the 'src' directory to the python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.Drag.Component import Component
+
+class Fuselage(Component):
+    def __init__(self, interference_factor, geometry_params:dict[str, float], laminar_fraction, surface_reynolds_factor = 0.00000405):
+        surface_wetted, characteristic_length = 0 #TODO compute from geometry
+        
+        super().__init__(interference_factor, surface_wetted, characteristic_length, laminar_fraction, surface_reynolds_factor)
+
+    
+    def form_factor(self, mach):
+        #TODO: implement
+        pass
