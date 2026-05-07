8cc5d658f77700b8f7b07e836bbd02794161ef27
MarriccZwei <marricc7@gmail.com>
2026-05-07 17:09:05 +0200

    Create estimate_CD0.py

---
+++ src/Drag/estimate_CD0.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Add the 'src' directory to the python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.Drag.Component import Component
+
