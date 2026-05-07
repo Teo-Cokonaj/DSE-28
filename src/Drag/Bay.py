ffd1d8177db29725e990b38556801b1b85526918
MarriccZwei <marricc7@gmail.com>
2026-05-07 10:32:40 +0200

    Update Bay.py

--- src/Drag/Bay.py
+++ src/Drag/Bay.py
@@ -8,10 +8,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.Drag.Component import Component
 
 class Bay(Component):
-    def __init__(self, interference_factor, geometry_params:dict[str, float], laminar_fraction, surface_reynolds_factor = 0.00000405):
-        self.length_to_diameter = geometry_params['L']/geometry_params['D']
-        surface_wetted = geometry_params['L']*np.pi/4*geometry_params['D']**2  
-        super().__init__(interference_factor, surface_wetted, geometry_params['L'], laminar_fraction, surface_reynolds_factor)
+    def __init__(self, interference_factor, length:float, diameter:float, laminar_fraction, surface_reynolds_factor = 0.00000405):
+        self.length_to_diameter = length/diameter
+        surface_wetted = length*np.pi/4*diameter**2  
+        super().__init__(interference_factor, surface_wetted, length, laminar_fraction, surface_reynolds_factor)
 
     
     def form_factor(self, mach=None):
