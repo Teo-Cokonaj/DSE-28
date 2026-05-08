c835bf2c8c3739420175d5f4a80d2d6f451beb91
GuilhermeCLdS <guilhermecastro2201@gmail.com>
2026-05-08 13:35:13 +0200

    WIP skeleton ASB geometry

--- src/aerosandbox/geometry_WIP.py
+++ src/aerosandbox/geometry_WIP.py
@@ -1,1 +1,62 @@
 # First try at using geometry from aerosandbox - Guilherme
+
+import aerosandbox as asb
+import aerosandbox.numpy as np
+
+
+# Select airfoils
+
+    # Wing
+
+    # Tail
+
+# Airplane
+
+    # name
+
+    # cg location
+
+    # wings
+
+        
+
+        # name
+
+        # coordinate of LE
+
+        # symmetry condition ? (true or false)
+
+        # wing's cross ("X") sections (Root)
+
+                # chord
+
+                # twist
+
+                # Airfoils are blended between a given XSec and the next one.
+
+                # control surfaces symmetry and deflection
+
+        # wing's cross ("X") sections (mid)
+
+                # chord
+
+                # twist
+
+                # Airfoils are blended between a given XSec and the next one.
+
+                # control surfaces symmetry and deflection
+
+        # wing's cross ("X") sections (tip)
+
+                # chord
+
+                # twist
+
+                # airfoil
+
+    # tail
+
+    
+        
+
+
