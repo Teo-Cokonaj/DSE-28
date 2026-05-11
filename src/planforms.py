4fe9502c3ad497d20e20f0080f9ae7105248dc65
AdamJaneczech <anndyjan@seznam.cz>
2026-05-11 13:18:49 +0200

    addded initial aerosandbox plotting

--- src/planforms.py
+++ src/planforms.py
@@ -1,6 +1,7 @@
 import aerosandbox.numpy as np
 import aerosandbox as asb
 import objects.lifting_surface_planform as lsp
+import matplotlib.pyplot as plt
 g = 9.81 #m/s^2, gravitational acceleration
 
 def sweep_le_to_qc_deg(sweep_le_deg, aspect_ratio, taper):
@@ -53,4 +54,41 @@ print(f"Aspect Ratio: {HUGO_planform.aspect_ratio}")
 print(f"Span: {HUGO_planform.span}")
 print(f"Sweep at Quarter Chord: {np.degrees(HUGO_planform.sweep_quarter_rad)} degrees")
 print(f"Taper Ratio: {HUGO_planform.taper}")
-print(f"Tip Twist: {np.degrees(HUGO_planform.tip_twist_rad)} degrees")
+#--------------------------------------AEROSANDBOX PLOT--------------------------------------
+
+S_h = HUGO_planform.wing_area
+c_root_h = HUGO_planform.c_root
+c_tip_h = HUGO_planform.c_tip
+semi_span_h = HUGO_planform.span / 2
+
+Lambda_qc_rad_h = np.radians(Lambda_qc_h)
+
+x_tip_qc = semi_span_h * np.tan(Lambda_qc_rad_h)
+x_tip_le = x_tip_qc + 0.25 * c_root_h - 0.25 * c_tip_h
+
+wing_hugo = asb.Wing(
+    name="HUGO Wing",
+    symmetric=True,
+    xsecs=[
+        asb.WingXSec(
+            xyz_le=[0, 0, 0],
+            chord=c_root_h,
+            twist=0,
+            airfoil=asb.Airfoil("naca0012")
+        ),
+        asb.WingXSec(
+            xyz_le=[x_tip_le, semi_span_h, 0],
+            chord=c_tip_h,
+            twist=np.degrees(tip_twist_h),
+            airfoil=asb.Airfoil("naca0012")
+        )
+    ]
+)
+
+airplane_hugo = asb.Airplane(
+    name="HUGO",
+    wings=[wing_hugo]
+)
+
+airplane_hugo.draw_three_view()
+plt.show()
