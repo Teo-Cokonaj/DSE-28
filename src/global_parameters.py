d06f98069b1cc58aa9e66a7b78cd4b151163668b
Teo Cokonaj <teo.cokonaj@gmail.com>
2026-05-12 11:54:56 +0200

    Merge

--- src/global_parameters.py
+++ src/global_parameters.py
@@ -37,13 +37,8 @@ class Assumptions():
         self.OMEGA_GO_AROUND = np.pi / 60 # [rad/s] -> rate 1 coordinated turn
         self.MC=0.75 #cruise Mach number
         self.MD = 0.80 #ADSEE: in general, MD is 0.05M higher than MC
-<<<<<<< HEAD
-        self.positive_C_L_max=1.25 #CHANGE
-        self.negative_C_L_max=-1.25  #CHANGE
-=======
         self.positive_C_L_max_airfoil=1.25 #CHANGE
         self.negative_C_L_max_airfoil=-1.25 #CHANGE
->>>>>>> f8e39a1d072c8950497e009ccfdf7254df3804a2
         self.C_L_alpha = 0.5*2*np.pi #CHANGE
 
         self.airfield_length = 1000. #m #TODO check with the actual airport
