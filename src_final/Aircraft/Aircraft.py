858c10ca237280d31736ea253cddeae38153bf1e
Plamena Dimitrova <plamid04@gmail.com>
2026-06-04 13:58:45 +0200

    update MD test to use notebook. Fix landing gear CD0 nan values

--- src_final/Aircraft/Aircraft.py
+++ src_final/Aircraft/Aircraft.py
@@ -17,10 +17,10 @@ class Aircraft:
         self.fixed = fixed
         self.planforms = planforms
         
-        #self.CD0_go_around = CD0_from_cache("go_around", fixed.drag_components(False) + planforms, planforms[0].wing_area)
-        #self.CD0_takeoff = CD0_from_cache("takeoff", fixed.drag_components(True) + planforms, planforms[0].wing_area)
-        #self.CD0_mach_max = CD0_from_cache("mach_max", fixed.drag_components(False) + planforms, planforms[0].wing_area)
-        #self.CD0_cruise = CD0_from_cache("cruise", fixed.drag_components(False) + planforms, planforms[0].wing_area)
+        self.CD0_go_around = CD0_from_cache("go_around", fixed.drag_components(False) + planforms, planforms[0].wing_area)
+        self.CD0_takeoff = CD0_from_cache("takeoff", fixed.drag_components(True) + planforms, planforms[0].wing_area)
+        self.CD0_mach_max = CD0_from_cache("mach_max", fixed.drag_components(False) + planforms, planforms[0].wing_area)
+        self.CD0_cruise = CD0_from_cache("cruise", fixed.drag_components(False) + planforms, planforms[0].wing_area)
 
     def total_mass(self)->float:
         return self.fixed.mass + sum(planform.mass_cache for planform in self.planforms)
@@ -36,12 +36,7 @@ class Aircraft:
         return self.total_mass() * CONSTANTS.G0 / self.reference_wing_area()
     
     def thrust_to_weight(self)->float:
-        return Assumptions().thrust_available / self.total_mass()
-    
-        
-
-
-
+        return Assumptions().thrust_available / (self.total_mass() * CONSTANTS.G0)
     
     def mach_go_around(self, assumptions:Assumptions):
  
