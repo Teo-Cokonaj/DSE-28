88ba1ecc3b6c55bf713fe68794521eb498772f3c
MarriccZwei <marricc7@gmail.com>
2026-06-05 13:59:49 +0200

    tail works canard not

--- src_final/EmpennageSizing/EmpennageFinder.py
+++ src_final/EmpennageSizing/EmpennageFinder.py
@@ -12,27 +12,26 @@ class EmpennageFinder():
         self.fixed = fixed
 
   
-    @staticmethod
-    def _x_ac(self, main_wing:Planform, x_LE:float, tolerance:float=1e-4):
-        return (x_LE + main_wing.x_MAC + main_wing.MAC / 4) / main_wing.MAC
+    def _x_ac(self, main_wing:Planform, x_LE:float, number_of_sections:int=20):
+        return (x_LE + main_wing.aerodynamic_center(number_of_sections)) / main_wing.MAC
     
 
-    def _x_cg(self, planforms:list[Planform]):
-        x_cgs = np.array(len(planforms) + 1)
-        ms = np.array(len(planforms) + 1)
+    def _x_cg(self, planforms:list[Planform], x_LEs:list[float], fore:bool=True):
+        x_cgs = np.zeros(len(planforms) + 1)
+        ms = np.zeros(len(planforms) + 1)
 
         for i, planform in enumerate(planforms):
             if (planform.mass_cache is None) or (planform.x_cg_cache is None):
                 #TODO implement structural analysis
                 raise NotImplementedError("Planform Structural Analysis not implementedyet")
             else:
-                x_cgs[i] = planform.x_cg_cache
+                x_cgs[i] = planform.x_cg_cache + x_LEs[i]
                 ms[i] = planform.mass_cache
 
-        x_cgs[-1] = self.fixed.x_cg_min # as we are sizing for controllability
+        x_cgs[-1] = self.fixed.x_cg_min if fore else self.fixed.x_cg_max
         ms[-1] = self.fixed.mass #conservative, as it assumes maximum mass at foremost C.G.
 
-        return (np.sum(x_cgs * ms) / ms.sum()) / planforms[0].MAC
+        return (np.sum(x_cgs * ms) / ms.sum() - self.fixed.x_LE_wing) / planforms[0].MAC
 
 
     def find_planforms(self, main_wing:Planform, initial:float=.1, maxiter:int=50) -> list[Planform]:
--- src_final/EmpennageSizing/TailFinder.py
+++ src_final/EmpennageSizing/TailFinder.py
@@ -9,38 +9,48 @@ from Aircraft.Fixed import Fixed
 from EmpennageSizing.EmpennageFinder import EmpennageFinder
 
 class TailFinder(EmpennageFinder):
-    def __init__(self, fixed, AR_h:float=3., taper_h:float=.7, taper_v:float=.9):
+    def __init__(self, fixed, AR_h:float=3., taper_h:float=.7, taper_v:float=.9, SM=0.05):
         super().__init__(fixed)
         self.AR_h = AR_h
         self.taper_h = taper_h
         self.taper_v = taper_v
+        self.SM = SM
 
 
-    @njit
     def find_planforms(self, main_wing:Planform, initial:float=.1, maxiter:int=50, tolerance:float=1e-4) -> list[Planform]:
         x_ac_mac = self._x_ac(main_wing, self.fixed.x_LE_wing)
         ac_term = x_ac_mac  - main_wing.cm_quarter_chord / main_wing.positive_C_L_max
-        Sh_S = 0.1
+        Sh_S = initial
         
         for i in range(maxiter):
             horizontal_tail, vertical_tail = self._t_tail(main_wing, Sh_S)
+            x_LE_vertical_tail = self.fixed.x_LE_tail - vertical_tail.span / 2 * np.tan(vertical_tail.sweep_LE_rad)
             
             l_h_mac = self._x_ac(horizontal_tail, self.fixed.x_LE_tail) - x_ac_mac
             CL_h = - horizontal_tail.positive_C_L_max
-            x_LE_vertical_tail = self.fixed.x_LE_tail - vertical_tail.span / 2 * np.tan(vertical_tail.sweep_LE_rad)
-            x_cg_mac = self._x_cg([main_wing, horizontal_tail, vertical_tail])
-            Sh_S_new = (x_cg_mac - ac_term) / (CL_h * l_h_mac / main_wing.positive_C_L_max)
 
-            if abs(Sh_S_new - Sh_S) / Sh_S_new < tolerance:
+            x_cg_mac_min = self._x_cg([main_wing, horizontal_tail, vertical_tail], [self.fixed.x_LE_tail, self.fixed.x_LE_tail, x_LE_vertical_tail])
+            Sh_S_new_ctrl = (x_cg_mac_min - ac_term) / (CL_h * l_h_mac / main_wing.positive_C_L_max)
+
+            x_cg_mac_max = self._x_cg([main_wing, horizontal_tail, vertical_tail], [self.fixed.x_LE_tail, self.fixed.x_LE_tail, x_LE_vertical_tail], False)
+            Sh_S_new_stab = (x_cg_mac_max - x_ac_mac - self.SM) / (horizontal_tail.CL_alpha * l_h_mac / main_wing.CL_alpha)
+
+            Sh_S_new = max(Sh_S_new_ctrl, Sh_S_new_stab)
+            assert Sh_S_new > 0
+
+            diff = abs(Sh_S_new - Sh_S) / Sh_S_new
+            if  diff < tolerance:
                 Sh_S = (Sh_S + Sh_S_new) / 2
                 break
             Sh_S = Sh_S_new
+
+            if i == maxiter - 1:
+                Warning(f"For planform of AR:{main_wing.aspect_ratio}, and sweep: {np.rad2deg(main_wing.sweep_quarter_rad)} deg, the tail sizing converged within {diff}, above tolerance: {tolerance}.")
 
         horizontal_tail, vertical_tail = self._t_tail(main_wing, Sh_S)
         return [horizontal_tail, vertical_tail]
 
 
-    @njit
     def _t_tail(self, main_wing:Planform, Sh_S:float)->tuple[Planform, Planform]:
         horizontal_tail = Planform(
                 aspect_ratio=self.AR_h, 
@@ -51,13 +61,17 @@ class TailFinder(EmpennageFinder):
                 cm_quarter_chord=0.,
                 wetted_surface_ratio=1.05,
                 interference_factor=1.04,
-                clmax=-.35 * self.AR_h**(1/3) / .9 / np.cos(main_wing.sweep_quarter_rad),
+                clmax=.35 * self.AR_h**(1/3) / .9 / np.cos(main_wing.sweep_quarter_rad),
                 flap=False
             )
+        horizontal_tail.x_cg_cache = horizontal_tail.x_MAC + horizontal_tail.MAC / 3 #TODO: rough assumption revise
+        horizontal_tail.mass_cache = 0.5 #TODO actually conduct the structural analysishere
+
         AR_v = 2 * horizontal_tail.span / (1 + 1/self.taper_v) / horizontal_tail.c_root #NOTE: to fit the horizontal tail
         vertical_tail = Planform(
             aspect_ratio=AR_v, 
             span=horizontal_tail.span,
+            taper=self.taper_v,
             sweep_quarter_deg=np.rad2deg(main_wing.sweep_quarter_rad),
             thickness_to_chord=0.12,
             cm_quarter_chord=0.,
@@ -66,5 +80,7 @@ class TailFinder(EmpennageFinder):
             clmax=0.,
             flap=False
         )
+        vertical_tail.x_cg_cache = vertical_tail.x_MAC + vertical_tail.MAC / 3 #TODO: rough assumption revise
+        vertical_tail.mass_cache = 0.3 #TODO actually conduct the structural analysishere
 
         return horizontal_tail, vertical_tail
