d55708669b199f7ffe2ab32a209ab66f8177faf1
CarlVistisen <carlv@Carl.>
2026-06-09 17:18:18 +0200

    updated and improved noise-calculator

--- Noise/ConstantsForNoisemodelling.py
+++ Noise/ConstantsForNoisemodelling.py
@@ -1,9 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src_final.global_parameters import *
 
-V=50
-h=10
 theta=0
 phi=0
 S=2
 b=5
-nu=1.5*10**(0-5)
+nu=CONSTANTS.DYNAMIC_VISCOSITY_SEA_LEVEL 
 
