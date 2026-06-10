978eea28da878a3b91f79b34cc9ddbda52209cf9
MarriccZwei <marricc7@gmail.com>
2026-06-10 15:11:02 +0200

    Update aileron_sizing.py

--- src_final/AileronSizing/aileron_sizing.py
+++ src_final/AileronSizing/aileron_sizing.py
@@ -56,9 +56,9 @@ def compute_p(b1, b2, delta_a_max, V, chords_ratio, n_sections, planform:Planfor
 
     P = -C_l_delta_a / C_l_p * delta_a_max * (2 * V / b)
 
-    print("C_l_delta_a: ", C_l_delta_a)
-    print("C_l_p: ", C_l_p)
-    print("P in degrees: ", np.degrees(P))
+    # print("C_l_delta_a: ", C_l_delta_a)
+    # print("C_l_p: ", C_l_p)
+    # print("P in degrees: ", np.degrees(P))
     return P
 
 
@@ -67,13 +67,13 @@ def size_ailerons(delta_a_max, Vmin, Vmax, chords_ratio, n_sections, planform:Pl
     halfspan = planform.span / 2
     quarterspan = planform.span / 4
     aileron_opt:list[tuple[float, float]] = [(y_fus, halfspan)]
-    tot_length_current = halfspan
+    tot_length_current = halfspan - y_fus
 
     #option one - single aileron
     for i, b1 in enumerate(np.linspace(y_fus, halfspan, n_sections)):
-        for b2 in np.linspace(b1, halfspan, n_sections-i):
-            roll_rate_min_computed = compute_p(b1, b2, delta_a_max, Vmin, chords_ratio, n_sections, planform)
-            roll_rate_max_computed = compute_p(b1, b2, delta_a_max, Vmax, chords_ratio, n_sections, planform)
+        for b2 in np.linspace(b1, halfspan, n_sections-i)[1:]:
+            roll_rate_min_computed = compute_p(b1, b2, delta_a_max, Vmin, chords_ratio, n_sections * 2, planform)
+            roll_rate_max_computed = compute_p(b1, b2, delta_a_max, Vmax, chords_ratio, n_sections * 2, planform)
             if (roll_rate_min_computed > roll_rate_min) and (roll_rate_max > roll_rate_max_computed) and ((b2 - b1) < tot_length_current):
                 aileron_opt = [(b1, b2)]
                 tot_length_current = b2 - b1
@@ -84,19 +84,19 @@ def size_ailerons(delta_a_max, Vmin, Vmax, chords_ratio, n_sections, planform:Pl
     inboard_aileron:tuple[float, float] = (y_fus, quarterspan)
     tot_length_inboard = quarterspan - y_fus
     for i, b1 in enumerate(np.linspace(0, quarterspan, n_sections //2)):
-        for b2 in np.linspace(b1, quarterspan, n_sections //2 - i):
-            roll_rate_computed = compute_p(b1, b2, delta_a_max, Vmax, chords_ratio, n_sections, planform)
+        for b2 in np.linspace(b1, quarterspan, n_sections //2 - i)[1:]:
+            roll_rate_computed = compute_p(b1, b2, delta_a_max, Vmax, chords_ratio, n_sections* 2, planform)
             if (roll_rate_max > roll_rate_computed > roll_rate_min) and ((b2-b1) < tot_length_inboard):
-                inboard_aileron = (b2, b1)
-                tot_length_current = b2 - b1
+                inboard_aileron = (b1, b2)
+                tot_length_inboard = b2 - b1
                 break
 
     #outboard_aileron for lowspeed
     for i, b1 in enumerate(np.linspace(quarterspan, halfspan, n_sections //2)):
-        for b2 in np.linspace(b1, halfspan, n_sections //2 - i):
-            roll_rate_computed = compute_p(b1, b2, delta_a_max, Vmin, chords_ratio, n_sections, planform)
+        for b2 in np.linspace(b1, halfspan, n_sections //2 - i)[1:]:
+            roll_rate_computed = compute_p(b1, b2, delta_a_max, Vmin, chords_ratio, n_sections * 2, planform)
             if (roll_rate_computed > roll_rate_min) and ((b2 - b1 + tot_length_inboard) < tot_length_current):
-                aileron_opt = [inboard_aileron, (b2, b1)]
+                aileron_opt = [inboard_aileron, (b1, b2)]
                 tot_length_current = tot_length_inboard + b2 - b1
                 break
 
@@ -111,22 +111,48 @@ def size_ailerons(delta_a_max, Vmin, Vmax, chords_ratio, n_sections, planform:Pl
 
 
 
+if __name__ == "__main__":
+    # Objective: find tau, b1, and b2 for the given wing parameters
+    planform = Planform(
+        aspect_ratio=27.0,
+        span=2.66,
+        sweep_quarter_deg=15.0,
+        taper=0.5,
+        thickness_to_chord=0.12,
+        cm_quarter_chord=0.0,
+        wetted_surface_ratio=1.07,
+        interference_factor=1.0,
+        clmax=1.44,
+        flap=False,
+    )
+    planform.add_cache_entry("takeoff", mach=0.147, altitude=0.0)
 
-# Objective: find tau, b1, and b2 for the given wing parameters
-planform = Planform(
-    aspect_ratio=27.0,
-    span=2.66,
-    sweep_quarter_deg=15.0,
-    taper=0.5,
-    thickness_to_chord=0.12,
-    cm_quarter_chord=0.0,
-    wetted_surface_ratio=0.9,
-    interference_factor=1.0,
-    clmax=1.44,
-    flap=False,
-)
-planform.add_cache_entry("takeoff", mach=0.147, altitude=0.0)
+    b1 = planform.half_span * 0.375
+    b2 = planform.half_span * 0.625
+    print("Roll rate (P):", compute_p(b1, b2, np.deg2rad(20.00), 50.0, 0.2, 1000, planform))
 
-b1 = planform.half_span * 0.375
-b2 = planform.half_span * 0.625
-print("Roll rate (P):", compute_p(b1, b2, np.deg2rad(20.00), 50.0, 0.2, 1000, planform))
+    ailerons = size_ailerons(np.deg2rad(20), 50., 200., 0.3, 60, planform, np.deg2rad(45), np.deg2rad(180), 0.33)
+    print(ailerons)
+    inb_aileron = ailerons[0]
+    oub_aileron = ailerons[-1]
+    print(np.degrees(compute_p(inb_aileron[0], inb_aileron[1], np.deg2rad(20.00), 200.0, 0.3, 120, planform)))
+    print(np.degrees(compute_p(oub_aileron[0], oub_aileron[1], np.deg2rad(20.00), 50.0, 0.3, 120, planform)))
+
+    #plot of aileron
+    stall_speed = 46.
+    maximum_speed = np.linspace(50., 250., 20)
+
+    ailerons = [size_ailerons(np.deg2rad(20), stall_speed, Vmax, 0.3, 300, planform, np.deg2rad(45), np.deg2rad(180), 0.33) for Vmax in maximum_speed]
+    b1_inb = [aileron[0][0] for aileron in ailerons]
+    b2_inb = [aileron[0][1] for aileron in ailerons]
+    b1_oub = [aileron[-1][0] for aileron in ailerons]
+    b2_oub = [aileron[-1][1] for aileron in ailerons]
+
+    plt.plot(maximum_speed, b1_inb, label="b1 inboard")
+    plt.plot(maximum_speed, b2_inb, label="b2 inboard")
+    plt.plot(maximum_speed, b1_oub, label="b1 outboard")
+    plt.plot(maximum_speed, b2_oub, label="b1 outboard")
+    plt.legend()
+    plt.show()
+    
+    
